10f594509964c05ed5de5b97ce1686676200042b
mujibur-rohman <muji.official06@gmail.com>
2023-09-06 01:16:00 +0700

    animation theme

--- src/components/atom/switch-theme.tsx
+++ src/components/atom/switch-theme.tsx
@@ -1,20 +1,44 @@
 import { cn } from "@/lib/utils";
-import { MoonIcon, SunIcon } from "@heroicons/react/20/solid";
+import { SunIcon } from "@heroicons/react/20/solid";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 type Props = {
   className?: string;
 };
 
 function SwitchTheme({ className }: Props) {
+  const [toggle, setToggle] = useState(false);
   return (
-    <button
+    <motion.button
+      variants={{
+        night: {
+          y: [0, -20, 100, 0],
+          transition: { duration: 2 },
+        },
+        light: {
+          y: [0, -20, 100, 0],
+          transition: { duration: 2 },
+        },
+      }}
+      animate={toggle ? "night" : "light"}
       className={cn(
         "switch-theme-mobile dark:text-neutral-100 bg-white transition-colors p-2 rounded-full cursor-pointer border-[1px] hover:dark:bg-neutral-700 dark:bg-neutral-600 shadow-lg shadow-neutral-100/5 dark:border-neutral-400",
         className
       )}
     >
-      <SunIcon className="w-6 h-6" />
-    </button>
+      <motion.div
+        onClick={() => {
+          setToggle(!toggle);
+        }}
+        animate={{
+          rotate: [0, 30, 0],
+          transition: { repeat: Infinity, repeatDelay: 1 },
+        }}
+      >
+        <SunIcon className="w-6 h-6" />
+      </motion.div>
+    </motion.button>
   );
 }
 
