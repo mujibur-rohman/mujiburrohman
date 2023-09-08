f42bf9ce5aa0929d3d02ef622d866e5c5b21f8c5
mujibur-rohman <muji.official06@gmail.com>
2023-09-09 01:22:06 +0700

    lanjut hero

--- src/components/pages/hero.tsx
+++ src/components/pages/hero.tsx
@@ -1,4 +1,5 @@
 import useInView from "@/hooks/useInView";
+import { CSSProperties } from "react";
 
 type Props = {};
 
@@ -8,9 +9,27 @@ function Hero({}: Props) {
     <section
       ref={ref}
       id="home"
-      className="min-h-screen flex justify-center items-center border-b-2"
+      className="min-h-screen flex justify-center items-center"
     >
-      <h1 className="text-2xl">Hero</h1>
+      <div className="font-bold px-10">
+        <h1 className="text-[2rem]">
+          Hi there <span>&#128075;&#127996;</span>
+        </h1>
+        <h1 className="text-[2rem]">
+          I'm{" "}
+          <span className="relative before:right-0 before:bottom-1 before:content-[''] before:block before:absolute before:w-[50%] before:h-3 before:bg-primary-blue-500">
+            <span className="relative">Mujiburrohman</span>
+          </span>
+        </h1>
+        <h1 className="font-normal mt-4">
+          A self-taught Frontend Developer who is currently focusing and diving
+          into the world of React.js, Next.js, and other related technologies.
+        </h1>
+
+        <div className="border-gradient">
+          <p className="">Hello, gradient</p>
+        </div>
+      </div>
     </section>
   );
 }
